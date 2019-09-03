06375f3b934fdfd94cce44ae71567b1325817685
Evgeniy Efimchenko <evgeniy@efimchenko.ru>
2019-09-03 13:16:31 +0300

    update admin

--- app/admin/index.php
+++ app/admin/index.php
@@ -17,14 +17,20 @@ Class Controller_index Extends Controller_Base {
         /* model */
         $this->load_model('m_index', array($this->logged));
         /* search user data */
+		$user_data = $this->models['m_index']->data;
+		foreach ($user_data as $name => $val) {
+			$this->view->set($name, $val);
+		}		
         /* view */
-        $this->show_view();
         $this->html = $this->view->read('v_dashboard');
         /* layouts */
         $this->parameters_layout["layout_content"] = $this->html;
         $this->parameters_layout["layout"] = 'dashboard';
         $this->parameters_layout["add_script"] .= '<script src="' . $this->get_path_controller() . '/admin/js/dashboard.js" type="text/javascript" /></script>';
         $this->parameters_layout["title"] = 'Административная панель/Главная';
+		$this->parameters_layout["description"] = 'EE_FRAMEFORK - Лёгкий PHP MVC фреймворк';
+		$this->parameters_layout["canonical_href"] = ENV_URL_SITE . '/admin';
+		$this->parameters_layout["keywords"] = Sysclass::keywords($this->html);
         $this->show_layout($this->parameters_layout);
     }
 }
