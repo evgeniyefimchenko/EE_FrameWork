5fbfde5c49b5acfd802e1237480bf90c8a69635f
Evgeniy Efimchenko <evgeniy@efimchenko.ru>
2019-09-05 15:58:03 +0300

    formatted

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
+
 /**
-* EE_FrameWork 1.0
-* Простой фреймворк построенный на базе схемы разделения данных приложения MVC
-* С русскими комментариями кода
-* Написан на языке PHP 5.6+ и использует следующие библиотеки
-* Bootstrap 4 https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css
-* jQuery latest http://code.jquery.com/jquery-latest.min.js
-* FontAwesom latest https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css 
-* В EeFramework используются следующие плагины
-* php_libmail_2_11 http://webi.ru/webi_files/php_libmail.html - работа с почтой
-* SafeMySQL http://phpfaq.ru/safemysql (под шаблон проектирования Singleton) - работа с БД MySql
-* Автор efimchenko.ru
-*/
+ * EE_FrameWork 1.0
+ * Простой фреймворк построенный на базе схемы разделения данных приложения MVC
+ * С русскими комментариями кода
+ * Написан на языке PHP 5.6+ и использует следующие библиотеки
+ * Bootstrap 4 https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css
+ * jQuery latest http://code.jquery.com/jquery-latest.min.js
+ * FontAwesom latest https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css 
+ * В EeFramework используются следующие плагины
+ * php_libmail_2_11 http://webi.ru/webi_files/php_libmail.html - работа с почтой
+ * SafeMySQL http://phpfaq.ru/safemysql (под шаблон проектирования Singleton) - работа с БД MySql
+ * Автор efimchenko.ru
+ */
 if (version_compare(phpversion(), '5.5.0', '<') == true) {
     die('PHP - Нужна версия >= 5.6.0');
 }
