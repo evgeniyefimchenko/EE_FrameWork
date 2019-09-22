6a51123ff08af362736786c1bd5a4c22c3b22414
Евгений Ефимченко <evgeniy@efimchenko.ru>
2019-09-22 12:59:44 +0300

    modified:   inc/configuration.php modified:   inc/startup.php

--- inc/startup.php
+++ inc/startup.php
@@ -1,12 +1,20 @@
 <?php
 
+$console = '';
 /**
  * Файл загрущик констант проекта и автолоадер классов php
  * автолоадер не чувствителен к регистру символов имён файлов
  * @author Evgeniy Efimchenko efimchenko.ru
  */
+ 
+/**
+* Включить необходимую конфигурационную информацию
+*/
 include_once 'configuration.php';
 
+/**
+* Определить константы для конфигурационной информации
+*/
 foreach ($C as $name => $val) {
     define($name, $val);
 }
@@ -23,32 +31,41 @@ if (ENV_SITE_INDEX !== 'ALL') {
 	// Для снятия лишней нагрузки файл robots.txt, при включении индексации, редактируется вручную (User-agent:*)
 }
 
+/**
+* Автолоадер
+*/
 spl_autoload_register(function($class_name) {
     $filename = $class_name . '.php';
     $res = search_file('classes', $filename);
     if ($res) {
         if (ENV_TEST) {
             echo 'include:' . $res . ' | ';
-        }		
-        include_once ($res);
+        }
     } else {
         echo 'Класс не найден: ' . $class_name . '<br/>пути поиска: ' . $console . '~';
         return FALSE;
     }
 });
 
+/**
+* Рекурсивный поиск файла класса
+*/
 function search_file($dir, $tosearch) {
-    global $console;	
+    global $console;
     $files = array_diff(scandir($dir), Array(".", ".."));
     foreach ($files as $d) {
         $path = $dir . "/" . $d;
         if (!is_dir($path)) { // Это не папка
-            if (strtolower($d) == strtolower($tosearch)) {
-                return $path;
+            if (strtolower($d) == strtolower($tosearch)) { // Файл найден
+                include_once($path);
+				$info = pathinfo($path);
+				if (class_exists($info['filename'])) { // Класс найден
+					return $path;
+				}				
             }
         } else { // Это папка продолжаем рекурсию
-		    $console .= $path . '<br/>';
-            $res = search_file($dir . "/" . $d, $tosearch);
+            $console .= $path . '<br/>';
+            $res = search_file($path, $tosearch);
             if ($res) {
                 return $res;
             }
